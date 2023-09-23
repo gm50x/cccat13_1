79ce26107a067e8bfb3b81d5a28e4b2c5bd8ec04
gm50x <getuliomagela@outlook.com>
2023-09-23 18:58:02 -0300

    adds extra cpf checks

--- backend/ride/test/CpfValidator.test.ts
+++ backend/ride/test/CpfValidator.test.ts
@@ -1,12 +1,16 @@
 import CpfValidator from "../src/CpfValidator";
 
-test.each(["95818705552", "01234567890", "565.486.780-60", "147.864.110-00"])(
-  "Deve validar um cpf",
-  function (cpf: string) {
-    const cpfValidator = new CpfValidator();
-    expect(cpfValidator.validate(cpf)).toBeTruthy();
-  }
-);
+test.each([
+  "95818705552",
+  "01234567890",
+  "565.486.780-60",
+  "147.864.110-00",
+  "081.438.020-43",
+  "002.580.820-62",
+])("Deve validar um cpf", function (cpf: string) {
+  const cpfValidator = new CpfValidator();
+  expect(cpfValidator.validate(cpf)).toBeTruthy();
+});
 
 test.each(["958.187.055-00", "958.187.055"])(
   "Não deve validar um cpf",
